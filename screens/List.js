e58f71fbd1462a05146a73a55be060d483245adb
anjaneyuluk-mls <anjaneyulu.kinnara@metrolabsservices.com>
2022-12-28 19:16:03 +0530

    add post call

--- screens/List.js
+++ screens/List.js
@@ -16,23 +16,27 @@ import {
 } from 'react-native';
 import {Text, Card, Button, Icon} from '@rneui/themed';
 import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Input} from '@rneui/base';
 
 const List = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const [data, setData] = useState(null);
+  const [name, setName] = useState('');
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  useEffect(() => {
+  const fetchMovies = () => {
     fetch('http://10.0.2.2:3600/movies')
       .catch(err => console.log(err))
       .then(res => res.json())
       .then(data => setData(data.items));
+  };
+  useEffect(() => {
+    fetchMovies();
   }, []);
   const renderItem = ({item, index}) => {
-    console.log(item);
     return (
       <Card key={item.id}>
         <Card.Title>{item.name}</Card.Title>
@@ -42,6 +46,18 @@ const List = () => {
       </Card>
     );
   };
+  const add = async () => {
+    console.log('I am pressed');
+    const d = {name: name};
+    await fetch('http://10.0.2.2:3600/movie', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(d),
+    });
+    fetchMovies();
+  };
   return (
     <SafeAreaView style={backgroundStyle}>
       {/* <StatusBar
@@ -49,16 +65,24 @@ const List = () => {
         backgroundColor={backgroundStyle.backgroundColor}
       /> */}
       <View style={styles.container}>
-        {data ? (
-          <FlatList
-            style={{width: '100%'}}
-            ListHeaderComponent={() => <Text style={styles.title}>Movies</Text>}
-            renderItem={renderItem}
-            data={data}
-          />
-        ) : (
-          <Text>loading</Text>
-        )}
+        <View style={{width: '100%', height: 120}}>
+          <Input placeholder="type name" onChangeText={setName} />
+          <Button onPress={add}>add</Button>
+        </View>
+        <View style={{width: '100%', height: 300}}>
+          {data ? (
+            <FlatList
+              style={{width: '100%'}}
+              ListHeaderComponent={() => (
+                <Text style={styles.title}>Movies</Text>
+              )}
+              renderItem={renderItem}
+              data={data}
+            />
+          ) : (
+            <Text>loading</Text>
+          )}
+        </View>
       </View>
     </SafeAreaView>
   );
